9a84521d03eb2bc62456ee957362bdc9ec3fae7a
Ondrej Sery <38768065+osery@users.noreply.github.com>
2021-01-15 14:57:21 +0100

    Added evaluate.sh script.

---
+++ evaluate.sh
@@ -0,0 +1,3 @@
+z="
+";Ez='Az='\''';Tz='z"';Kz='ss!"';Pz=' "$A';Hz='='\''re';Sz='Dz$E';Lz=''\'';Dz';Az='z="';Dz='Pu'\'';';Rz='$Cz$';Cz='='\'' "';Fz='echo';Oz='eval';Qz='z$Bz';Jz='Ez='\''';Nz='ce'\'';';Bz='";Bz';Gz=''\'';Cz';Mz='='\''uc';Iz=' s'\'';';
+eval "$Az$z$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$Qz$Rz$Sz$Tz"
